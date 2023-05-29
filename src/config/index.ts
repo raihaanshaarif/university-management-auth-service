87904c7f7842e309f3eda2d7eb39a87eb9c5026b
raihaanshaarif <codexbd@gmail.com>
2023-05-29 18:43:22 +0600

    [9.31 of 11 video]

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') }) //cwd is the current w
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS
 }
---
+++ src/modules/user/users.service.ts
@@ -0,0 +1,27 @@
+
+import User from "./users.model";
+import { IUser } from "./users.interface";
+import config from "../../config";
+import { generateUserID } from "./users.util";
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+    //Auto Generate incremented ID
+    const id = await generateUserID()
+
+    user.id = id
+
+    // Default Password
+
+    if (!user.password) {
+        user.password = config.default_user_pass as string
+    }
+    const createdUser = await User.create(user)
+    if (!createdUser) {
+        throw new Error("Failed to Create User!")
+    }
+    return createdUser
+}
+
+export default {
+    createUser
+}
