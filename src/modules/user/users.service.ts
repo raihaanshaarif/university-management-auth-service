88ed25d37f37bbf07a22be5f682d276a955d1909
Raihan Sharif <codexbd@gmail.com>
2023-06-06 11:17:22 +0600

    [Donne]vscodeconfig added

--- src/modules/user/users.service.ts
+++ src/modules/user/users.service.ts
@@ -1,26 +1,26 @@
-import User from "./users.model";
-import { IUser } from "./users.interface";
-import config from "../../config";
-import { generateUserID } from "./users.util";
+import User from './users.model'
+import { IUser } from './users.interface'
+import config from '../../config'
+import { generateUserID } from './users.util'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-    //Auto Generate incremented ID
-    const id = await generateUserID()
+  //Auto Generate incremented ID
+  const id = await generateUserID()
+  user.id = id
 
-    user.id = id
+  // Default Password
 
-    // Default Password
+  if (!user.password) {
+    user.password = config.default_user_pass as string
+  }
+  const createdUser = await User.create(user)
 
-    if (!user.password) {
-        user.password = config.default_user_pass as string
-    }
-    const createdUser = await User.create(user)
-    if (!createdUser) {
-        throw new Error("Failed to Create User!")
-    }
-    return createdUser
+  if (!createUser) {
+    throw new Error('Failed to Create User!')
+  }
+  return createdUser
 }
 
 export default {
-    createUser
+  createUser,
 }
